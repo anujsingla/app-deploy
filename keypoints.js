76252612d945514920ab80d42c3d467aea20525d
Anuj Singla <asingla@redhat.com>
2022-09-14 09:06:11 +0530

    code changes .

---
+++ keypoints.js
@@ -0,0 +1,32 @@
+/**
+
+How to build a GitHub Pages site?
+
+It helps to host website from your github repository. You can create a site
+or blog from your Github respositories with github pages.
+It is a static site hosting service from Github that takes HTML, CSS, Javscript
+files from repository.
+You can host your site on "github.io" domain or your own custom domain.
+
+With GitHub Pages, you can host project blogs, documentation, resumes,
+portfolios, or any other static content you'd like. Your GitHub repository
+can easily become its own website.
+
+http(s)://<username>.github.io/<repository>
+https://anujsingla.github.io/app-deploy
+
+https://pages.github.com/
+https://github.com/skills/github-pages
+
+Getting started doc:
+https://docs.github.com/en/pages/getting-started-with-github-pages/creating-a-github-pages-site
+
+Examples:
+https://github.com/collections/github-pages-examples
+
+Custom domain configuration:
+https://docs.github.com/en/pages/configuring-a-custom-domain-for-your-github-pages-site
+
+it is a service from Github that enables 
+
+ */
